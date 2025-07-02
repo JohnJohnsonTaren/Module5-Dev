acfc557e90b81952067a0d57719893c099743c71
JohnJohnsonTaren <61081845+JohnJohnsonTaren@users.noreply.github.com>
2025-07-02 16:37:00 +0300

    Add files via upload

--- src/main/java/org/example/Iteration/App_Iteration.java
+++ src/main/java/org/example/Iteration/App_Iteration.java
@@ -3,24 +3,30 @@ package org.example.Iteration;
 import java.util.Scanner;
 
 public class App_Iteration {
+    public  static int fib(int n) {
+        if (n <= 0) {
+            return n;
+        }
+
+        int a = 0;
+        int b = 1;
+        int nextFib = 0;
+
+        for (int i = 2; i <= n; i++) {
+            nextFib = a + b;
+            a = b;
+            b = nextFib;
+        }
+        return nextFib;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         scanner.close();
 
-        int a = 0;
-        int b = 1;
-
-        System.out.println("F(0) = " + a);
-        if (n >= 1) {
-            System.out.println("F(1) = " + b);
-
-            for (int i = 2; i <= n; ++i) {
-                int nextFib = a + b;
-                System.out.println("F(" + i + ") = " + nextFib);
-                a = b;
-                b = nextFib;
-            }
+        for (int i = 0; i <= n; ++i) {
+            System.out.println("F(" + i + ") = " + fib(i));
         }
     }
 }
